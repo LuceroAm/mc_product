b0635b1fc6473a6f9b4857fe363331de6651fc88
Lucero Ampudia Trigoso <lampudia@emeal.nttdata.com>
2022-01-07 13:37:26 -0500

    Feature se implementa el proyecto recien generado

---
+++ src/main/java/com/nttdata/bootcamp/product/ProductApplication.java
@@ -0,0 +1,13 @@
+package com.nttdata.bootcamp.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductApplication.class, args);
+	}
+
+}
