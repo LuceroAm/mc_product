05b7a0bdaa42886c1f40b891ae6091b2218152c7
Lucero Ampudia Trigoso <lampudia@emeal.nttdata.com>
2022-01-07 13:51:16 -0500

    Feature se implementa la estructura del proyecto

--- src/main/java/com/nttdata/bootcamp/ProductApplication.java
+++ src/main/java/com/nttdata/bootcamp/ProductApplication.java
@@ -1,4 +1,4 @@
-package com.nttdata.bootcamp.product;
+package com.nttdata.bootcamp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
