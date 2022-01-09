b7f2c317c9fe56089529e2ca8a13862503297179
Lucero Ampudia Trigoso <lampudia@emeal.nttdata.com>
2022-01-09 00:43:30 -0500

    Feature se implementa las clases necesarias para el proyecto

---
+++ src/main/java/com/nttdata/bootcamp/product/model/dto/request/ProductRequest.java
@@ -0,0 +1,4 @@
+package com.nttdata.bootcamp.product.model.dto.request;
+
+public class ProductRequest {
+}
---
+++ src/main/java/com/nttdata/bootcamp/product/model/dto/response/ProductResponse.java
@@ -0,0 +1,4 @@
+package com.nttdata.bootcamp.product.model.dto.response;
+
+public class ProductResponse {
+}
