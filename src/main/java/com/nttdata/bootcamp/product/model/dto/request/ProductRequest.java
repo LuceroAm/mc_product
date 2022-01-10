ade54a7dd286b05e402e9c72a54b45cfccd26850
Lucero Ampudia Trigoso <luamtri24@gmail.com>
2022-01-10 07:28:54 -0500

    feature se implementa la capa dto

--- src/main/java/com/nttdata/bootcamp/product/model/dto/request/ProductRequest.java
+++ src/main/java/com/nttdata/bootcamp/product/model/dto/request/ProductRequest.java
@@ -1,4 +1,55 @@
 package com.nttdata.bootcamp.product.model.dto.request;
 
+import com.nttdata.bootcamp.product.model.AccountType;
+import com.nttdata.bootcamp.product.model.Product;
+import com.nttdata.bootcamp.product.model.ProductType;
+import com.nttdata.bootcamp.product.model.TypeDocument;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.util.UUID;
+
+/**
+ * <b>Class</b>: {@link ProductRequest}<br/>
+ * @author NTTDATA <br/>
+ * <u>Developed by</u>: <br/>
+ * <ul>
+ * <li>L.A.T.</li>
+ * </ul>
+ * <u>Changes</u>:<br/>
+ * <ul>
+ * <li>06/01/2022 (acronym) Creation class.</li>
+ * </ul>
+ * @version 1.0
+ */
+@Data
+@ApiModel("Model ProductRequest")
 public class ProductRequest {
+    @ApiModelProperty(value = "id", position = 1)
+    private String id = UUID.randomUUID().toString();
+
+    @ApiModelProperty(value = "productType", position = 2)
+    private ProductType productType;
+
+    @ApiModelProperty(value = "accountType", position = 3)
+    private AccountType accountType;
+
+    @ApiModelProperty(value = "accountNumber", position = 4)
+    private String accountNumber;
+
+    @ApiModelProperty(value = "typeDocument", position = 5)
+    private TypeDocument typeDocument;
+
+    @ApiModelProperty(value = "numberDocument", position = 6)
+    private String numberDocument;
+
+    @ApiModelProperty(value = "cardNumber", position = 7)
+    private String cardNumber;
+
+    @ApiModelProperty(value = "password", position = 8)
+    private String password;
+
+    @ApiModelProperty(value = "amount", position = 9)
+    private String amount;
 }
--- src/main/java/com/nttdata/bootcamp/product/model/dto/response/ProductResponse.java
+++ src/main/java/com/nttdata/bootcamp/product/model/dto/response/ProductResponse.java
@@ -1,4 +1,38 @@
 package com.nttdata.bootcamp.product.model.dto.response;
 
+import com.nttdata.bootcamp.product.model.AccountType;
+import com.nttdata.bootcamp.product.model.ProductType;
+import com.nttdata.bootcamp.product.model.TypeDocument;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+/**
+ * <b>Class</b>: {@link ProductResponse}<br/>
+ * @author NTTDATA <br/>
+ * <u>Developed by</u>: <br/>
+ * <ul>
+ * <li>L.A.T.</li>
+ * </ul>
+ * <u>Changes</u>:<br/>
+ * <ul>
+ * <li>06/01/2022 (acronym) Creation class.</li>
+ * </ul>
+ * @version 1.0
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class ProductResponse {
+    private String id = UUID.randomUUID().toString();
+    private ProductType productType;
+    private AccountType accountType;
+    private String accountNumber;
+    private TypeDocument typeDocument;
+    private String numberDocument;
+    private String cardNumber;
+    private String password;
+    private String amount;
 }
