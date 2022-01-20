976710a4eac49fb2c7495149cfe07debfb37d00c
Lucero Ampudia Trigoso <luamtri24@gmail.com>
2022-01-20 16:47:28 -0500

    Se implementa logica de la api

--- src/main/java/com/nttdata/bootcamp/product/business/impl/ProductServiceImpl.java
+++ src/main/java/com/nttdata/bootcamp/product/business/impl/ProductServiceImpl.java
@@ -1,13 +1,10 @@
 package com.nttdata.bootcamp.product.business.impl;
 
 import com.nttdata.bootcamp.product.business.ProductService;
-import com.nttdata.bootcamp.product.model.Product;
 import com.nttdata.bootcamp.product.model.dto.request.ProductRequest;
 import com.nttdata.bootcamp.product.model.dto.response.ProductResponse;
 import com.nttdata.bootcamp.product.repository.ProductRepository;
 import com.nttdata.bootcamp.product.util.ProductBuilder;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -37,52 +34,26 @@ public class ProductServiceImpl implements ProductService {
     @Override
     public Mono<ProductResponse> create(ProductRequest productRequest) {
         log.info("Guardar datos del Producto");
-        return productRepository.save(setProduct(productRequest))
-                 .map(this::getProduct);
-//                 .map(product -> ProductBuilder
-//                         .productEntityToProductResponse(product));
-    }
-
-    private Product setProduct (ProductRequest request){
-        return Product.builder()
-                .id(request.getId())
-                .productType(request.getProductType())
-                .accountType(request.getAccountType())
-                .creditType(request.getCreditType())
-                .accountNumber(request.getAccountNumber())
-                .typeDocument(request.getTypeDocument())
-                .numberDocument(request.getNumberDocument())
-                .cardNumber(request.getCardNumber())
-                .password(request.getPassword())
-                .amount(request.getAmount())
-                .build();
-    }
-
-    private ProductResponse getProduct (Product model){
-        return ProductResponse.builder()
-                .productType(model.getProductType())
-                .accountType(model.getAccountType())
-                .creditType(model.getCreditType())
-                .accountNumber(model.getAccountNumber())
-                .typeDocument(model.getTypeDocument())
-                .numberDocument(model.getNumberDocument())
-                .cardNumber(model.getCardNumber())
-                .password(model.getPassword())
-                .amount(model.getAmount())
-                .build();
+        return productRepository.save(ProductBuilder
+                        .productRequestToProductEntity(productRequest))
+                 .map(product -> ProductBuilder
+                         .productEntityToProductResponse(product));
     }
 
     @Override
     public Mono<ProductResponse> findById(String id) {
         return productRepository.findById(id)
-                .map(this::getProduct);
+                .map(product -> ProductBuilder
+                        .productEntityToProductResponse(product));
     }
 
     @Override
     public Flux<ProductResponse> findAll() {
         log.info("Obtener todos los registros de Productos");
         return productRepository.findAll()
-                .map(this::getProduct);
+                .map(product -> ProductBuilder
+                        .productEntityToProductResponse(product));
+
     }
 
     @Override
@@ -100,7 +71,8 @@ public class ProductServiceImpl implements ProductService {
         log.info("Eliminar un registro de un Producto");
         return productRepository
                 .findById(id)
-                .map(this::getProduct)
+                .map(product -> ProductBuilder
+                        .productEntityToProductResponse(product))
                 .flatMap(productResponse ->  productRepository.deleteById(productResponse.getId())
                 .thenReturn(productResponse));
     }
